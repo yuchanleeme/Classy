a8a55d8b69c4292b03348ce35a28ed407e331d97
injeChoi <2015jasonc@gmail.com>
2020-10-07 12:09:16 +0000

    Re-do

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ var connection = new RTCMultiConnection();
 // this line is VERY_important
 connection.socketURL = 'https://rtcmulticonnection.herokuapp.com:443/';
 
+var predefinedRoomId = prompt('Please enter room-code');
+connection.openOrJoin(predefinedRoomId);
+
 connection.extra = {
     // divBGColor: prompt('Please enter DIV background color.') // 영역 배경 색깔 채우기
     fullName: prompt('Please Enter Your Full Name')
@@ -20,10 +23,10 @@ connection.sdpConstraints.mandatory = {
 
 var videosContainer = document.getElementById('section');
 connection.onstream = function(event) {
-	delete event.mediaElement.id; // make sure that below DIV has unique ID in the DOM
+    delete event.mediaElement.id; // make sure that below DIV has unique ID in the DOM
 	
     var div = document.createElement('div');
-	div.id = event.streamid;
+    div.id = event.streamid;
     div.className = 'video-div';
     div.appendChild(event.mediaElement); // appending VIDEO to DIV
 
@@ -31,8 +34,7 @@ connection.onstream = function(event) {
     // h2.innerHTML = event.extra.divBGColor;
     h2.innerHTML = event.extra.fullName;
     div.appendChild(h2);
-	
-	div.style.backgroundColor = event.extra.divBGColor;
+	// div.style.backgroundColor = event.extra.divBGColor;
 
     videosContainer.appendChild(div);
 };
@@ -43,9 +45,6 @@ connection.onstreamended = function(event) {
 		div.parentNode.removeChild( div ); // remove it from the DOM
 	}
 };
-
-var predefinedRoomId = prompt('Please enter room-code');
-connection.openOrJoin(predefinedRoomId);
 
 // consider the URL as UNIQUE-ROOM-ID
 // connection.openOrJoin(connection.channel);
