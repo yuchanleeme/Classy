d83240a8b11c0e9371250c6e252e39777f03a00b
injeChoi <2015jasonc@gmail.com>
2020-10-08 11:22:46 +0000

    New Screen Share API

---
+++ main_screen_handler.js
@@ -0,0 +1,60 @@
+/*!
+ *
+ * WebRTC Lab
+ * @author dodortus (dodortus@gmail.com / codejs.co.kr)
+ *
+ */
+//test
+$(function() {
+    console.log('Loaded Main');
+  
+    const screenHandler = new ScreenHandler();
+  
+    /**
+     * 비디오 엘리먼트에 재생을 위해 stream 바인딩
+     * @param data
+     */
+    function setVideoStream(data) {
+      const video = data.el;
+      video.srcObject = data.stream;
+    }
+  
+    /**
+     * 로컬 스트림 핸들링
+     * @param stream
+     */
+    function onLocalStream(stream) {
+      console.log('onLocalStream', stream);
+  
+      setVideoStream({
+        el: document.querySelector('#local-video'),
+        stream: stream,
+      });
+    }
+  
+    /**
+     * screenHandler를 통해 캡쳐 API를 호출합니다.
+     */
+    function startScreenShare() {
+      screenHandler.start((stream) => {
+        onLocalStream(stream);
+      });
+    }
+  
+    /**
+     * DOM 이벤트 바인딩
+     */
+    function bindEvent() {
+      document.querySelector('#btn-start').onclick = startScreenShare;
+    }
+  
+    /**
+     * 초기화
+     */
+    function initialize() {
+      bindEvent();
+    }
+  
+    initialize();
+  });
+  
