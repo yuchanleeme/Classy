2c190cf52a1cc6fbfe0d583d87f5b345694e782d
injeChoi <2015jasonc@gmail.com>
2020-12-13 10:30:59 +0900

    heroku second

--- server/index.js
+++ server/index.js
@@ -186,6 +186,6 @@ app.get('/api/users/logout', auth, (req, res) => {
     })
 })
 
-const port = 5000
+const port = process.env.PORT||5000
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
 // app.listen(process.env.PORT || port)
